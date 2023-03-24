117f18298a900fc3880a5a97e4d55d508fdb38a7
Алина Гайнанова <alinagainanova@MacBook-Air-Alina.local>
2023-03-24 14:31:14 +0400

    add 404 page style

--- src/components/NotFoundPage/index.jsx
+++ src/components/NotFoundPage/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Button';
+import "./NotFoundPage.scss";
 
 function NotFoundPage() {
   return (
-    <div className="not-found-page">
-      NotFoundPage
+    <div className="container-fluid not-found-page flex-column">
+      <div className="not-found-page__central d-flex flex-column justify-content-around align-items-center">
+        <div className="big-title error">404</div>
+        <div className="d-flex flex-column align-items-center ">
+           <div className="big-title error-text">page not found</div>
+           <div className="d-none d-md-block">it was removed or you mistyped the url</div>
+        </div>
+        <div className="d-md-none">it was removed or you mistyped the url</div>
+        <Link to="/" className="inside-link d-none d-md-block">
+          <Button text="return to homepage" />
+        </Link>
+      </div>
+      <Link to="/" className="outside-link d-md-none w-100">
+        <Button text="return to homepage" />
+      </Link>
     </div>
   );
 }
