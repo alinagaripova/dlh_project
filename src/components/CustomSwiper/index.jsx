dff8a1f63e1087378bffd1549a43d3e835c3e6f1
Алина Гайнанова <alinagainanova@MacBook-Air-Alina.local>
2023-04-07 03:55:33 +0400

    fix burger menu & scroll

--- src/components/CustomSwiper/index.jsx
+++ src/components/CustomSwiper/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Marquee from 'react-double-marquee';
+import Marquee from "react-fast-marquee";
 
 import './CustomSwiper.scss';
 
@@ -7,7 +7,7 @@ function CustomSwiper({iconsList}) {
   return (
     <div className="container-custom">
       <div style={{ whiteSpace: 'nowrap' }}>
-        <Marquee direction="left" >
+        <Marquee direction="left" gradient={false} >
           <div className="d-flex">
             {iconsList.map(({ img }) => (
               <div className="swiper-custom-element me-3">
